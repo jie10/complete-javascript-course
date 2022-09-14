49c98c155fc253c47ec5716cd1c656255d53958e
Benjie Jabir Burillo <benjieburillo10@gmail.com>
2022-09-15 06:29:44 +0800

    Type of Data Types

--- 01-Fundamentals-Part-1/starter/app.js
+++ 01-Fundamentals-Part-1/starter/app.js
@@ -6,15 +6,24 @@
 // String
 let firstName = "Jie"
 console.log(firstName)
+console.log(typeof firstName)
+
 // Number
 const num = 21
 console.log(num)
+console.log(typeof num)
+
 // Boolean
 let isTrue = true
 console.log(isTrue)
+console.log(typeof isTrue)
+
 // Undefined
 let isUndefined
 console.log(isUndefined)
+console.log(typeof isUndefined)
+
 // Null
 let isNull = null
 console.log(isNull)
+console.log(typeof isNull)
