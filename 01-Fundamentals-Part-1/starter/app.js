42cc4c18bb4fd0aa0785a4963606dd47bbce31f5
Benjie Jabir Burillo <benjieburillo10@gmail.com>
2022-09-14 22:33:51 +0800

    Initial commit

---
+++ 01-Fundamentals-Part-1/starter/app.js
@@ -0,0 +1,19 @@
+"use strict"
+
+// * Variables & Values
+
+// String
+let firstName = "Jie"
+console.log(firstName)
+// Number
+const num = 21
+console.log(num)
+// Boolean
+let isTrue = true
+console.log(isTrue)
+// Undefined
+let isUndefined
+console.log(isUndefined)
+// Null
+let isNull = null
+console.log(isNull)
